dcd78fed09db6ed35ed5bde62bac71e07aea777a
Emery <emerypakula@gmail.com>
2025-12-28 22:06:50 +0100

    window works and stretches properly + custom window border

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    frame: false,  
+    transparent: false,  
+    resizable: true,
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
-      nodeIntegration: true
+        preload: path.join(__dirname, 'preload.js'),
+        contextIsolation: true,
+        enableRemoteModule: false,
+        nodeIntegration: false
     }
+
   });
   win.loadFile('index.html');
 }
+
+const { ipcMain } = require('electron');
+
+ipcMain.handle('minimize', () => {
+  win.minimize();
+});
+
+ipcMain.handle('maximize', () => {
+  if (win.isMaximized()) {
+    win.unmaximize();
+  } else {
+    win.maximize();
+  }
+});
+
+ipcMain.handle('close', () => {
+  win.close();
+});
+
+ipcMain.on('start-resize', (event, edge) => {
+  const validEdges = ['bottom', 'top', 'left', 'right', 'top-left', 'top-right', 'bottom-left', 'bottom-right'];
+  if (validEdges.includes(edge)) {
+    win.setResizable(true);
+  }
+});
+
 
 app.whenReady().then(createWindow);
